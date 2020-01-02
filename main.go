da9e3a092b44b9b66c26793ba81579106582cc17
Kien Nguyen <kiennt65@viettel.com.vn>
2020-01-02 16:08:26 +0700

    Add main logic

--- main.go
+++ main.go
@@ -17,19 +17,55 @@ package main
 import (
 	"fmt"
 	"os"
+	"os/exec"
 	"os/user"
 	"path/filepath"
 	"regexp"
 	"runtime"
+	"strconv"
 
 	"github.com/pkg/errors"
 	"gopkg.in/alecthomas/kingpin.v2"
 )
 
 var (
-	repoSyntax = regexp.MustCompile(`^(?:git|ssh|https?|git@[-\w.]+):(\/\/)?(.*?)(\.git)(\/?|\#[-\d\w._]+?)$`)
-	workspace  = os.Getenv("WORKSPACE")
+	POSSIBLE_REGEXES = []*regexp.Regexp{
+		regexp.MustCompile(`^(?P<protocol>https?|git|ssh|rsync)\://` +
+			`(?:(?P<user>.+)@)*` +
+			`(?P<resource>[a-z0-9_.-]*)` +
+			`[:/]*` +
+			`(?P<port>[\d]+){0,1}` +
+			`(?P<path>\/((?P<owner>[\w\-]+)\/)?` +
+			`((?P<name>[\w\-\.]+?)(\.git|\/)?)?)$`),
+		regexp.MustCompile(`(git\+)?` +
+			`((?P<protocol>\w+)://)` +
+			`((?P<user>\w+)@)?` +
+			`((?P<resource>[\w\.\-]+))` +
+			`(?P<path>(\/(?P<owner>\w+)/)?` +
+			`(\/?(?P<name>[\w\-]+)(\.git|\/)?)?)$`),
+		regexp.MustCompile(`^(?:(?P<user>.+)@)*` +
+			`(?P<resource>[a-z0-9_.-]*)[:]*` +
+			`(?P<port>[\d]+){0, 1}` +
+			`(?P<path>\/?(?P<owner>.+)/(?P<name>.+).git)$`),
+		regexp.MustCompile(`((?P<user>\w+)@)?` +
+			`((?P<resource>[\w\.\-]+))` +
+			`[\:\/]{1, 2}` +
+			`(?P<path>((?P<owner>\w+)/)?` +
+			`((?P<name>[\w\-]+)(\.git|\/)?)?)$`),
+	}
+	WORKSPACE = os.Getenv("WORKSPACE")
 )
+
+// Repo represents a repository structure.
+type Repo struct {
+	Protocol string
+	User     string
+	Resource string
+	Port     string
+	Path     string
+	Owner    string
+	Name     string
+}
 
 func main() {
 	if os.Getenv("DEBUG") != "" {
@@ -39,7 +75,7 @@ func main() {
 
 	a := kingpin.New(filepath.Base(os.Args[0]), "A lazy tool written by Golang to clone git repository then place it to the right folder.")
 	a.HelpFlag.Short('h')
-	repo := a.Arg("repo", "Repository URL, for example: git@github.com:x/y.git, https://github.com/x/y.git...").Required().String()
+	rawRepoPtn := a.Arg("repository", "Repository URL, for example: git@github.com:x/y.git, https://github.com/x/y.git...").Required().String()
 	_, err := a.Parse(os.Args[1:])
 	if err != nil {
 		fmt.Fprintln(os.Stderr, errors.Wrapf(err, "Error parsing commandline arguments"))
@@ -47,30 +83,94 @@ func main() {
 		os.Exit(2)
 	}
 
+	rawRepo := *rawRepoPtn
 	// Verify URL
-	if err := verifyRepo(*repo); err != nil {
-		fmt.Fprintln(os.Stderr, err)
+	repo, err := parseRepo(rawRepo)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, errors.Wrapf(err, "Error parsing the input repository URL"))
 		os.Exit(2)
 	}
 
 	// Setup directory
-	if workspace == "" {
-		curUsr, err := user.Current()
-		if err != nil {
-			fmt.Fprintln(os.Stderr, err)
-			os.Exit(2)
-		}
-		workspace = filepath.Join(curUsr.HomeDir, "Workspace")
+	curUsr, err := user.Current()
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(2)
 	}
-	if _, err := os.Stat(workspace); os.IsNotExist(err) {
-		os.MkdirAll(workspace, os.ModePerm)
+	if WORKSPACE == "" {
+		WORKSPACE = filepath.Join(curUsr.HomeDir, "Workspace")
 	}
+
+	// Create repository folder if not exist
+	// Repository folder: $WORKSPACE/<resource>/<owner>/<name>
+	// For example: $WORKSPACE/github.com/ntk148v/gclone
+	dir := filepath.Join(WORKSPACE, repo.Resource, repo.Path)
+	if _, err := os.Stat(dir); os.IsNotExist(err) {
+		os.MkdirAll(dir, 0755)
+		uid, _ := strconv.Atoi(curUsr.Uid)
+		gid, _ := strconv.Atoi(curUsr.Gid)
+		os.Chown(dir, uid, gid)
+	}
+
+	cmd := exec.Command("git", "clone", rawRepo, dir)
+	cmd.Dir = dir
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	err = cmd.Run()
+	if err != nil {
+		fmt.Fprintln(os.Stderr, errors.Wrapf(err, fmt.Sprintf("Error cloning %s to directory %s", rawRepo, dir)))
+		os.Remove(dir)
+		os.Exit(2)
+	}
+
+	fmt.Printf("Repository %s is cloned to %s\n", repo.Path, dir)
+	// TODO(kiennt): Find a way to cd into the directory :( os.Chdir doesn't work at all.
 }
 
-func verifyRepo(repo string) error {
-	matched := repoSyntax.MatchString(repo)
-	if !matched {
-		return errors.New("An invalid git repository url")
+func parseRepo(rawRepo string) (*Repo, error) {
+	for i := 0; i < 4; i++ {
+		match := POSSIBLE_REGEXES[i].FindAllStringSubmatch(rawRepo, -1)
+		if len(match) != 0 {
+			m := match[0]
+			switch i {
+			case 0:
+				return &Repo{
+					Protocol: m[1],
+					User:     m[2],
+					Resource: m[3],
+					Port:     m[4],
+					Path:     m[5],
+					Owner:    m[6],
+					Name:     m[7],
+				}, nil
+			case 1:
+				return &Repo{
+					Protocol: m[1],
+					User:     m[2],
+					Resource: m[3],
+					Path:     m[4],
+					Owner:    m[5],
+					Name:     m[6],
+				}, nil
+			case 2:
+				return &Repo{
+					User:     m[1],
+					Resource: m[2],
+					Port:     m[3],
+					Path:     m[4],
+					Owner:    m[5],
+					Name:     m[6],
+				}, nil
+			case 3:
+				return &Repo{
+					User:     m[1],
+					Resource: m[2],
+					Path:     m[3],
+					Owner:    m[4],
+					Name:     m[5],
+				}, nil
+			}
+		}
 	}
-	return nil
+	return nil, errors.New(fmt.Sprintf("Invalid repository URL: %s", rawRepo))
 }
